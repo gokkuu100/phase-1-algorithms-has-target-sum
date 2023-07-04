0688253f8c1b738fffb9cec60798f4ec220e73de
goku <wkurts247@gmail.com>
2023-07-04 18:14:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  time capacity 0(n*n) 0(n^2)
 */
 
 /* 
   Add your pseudocode here
+  for loop array
+  minus target from array
+  for loop through the remaining array
 */
 
 /*
   Add written explanation of your solution here
+  for loop through the entire array checking numbers
+  intialize a variable which stores the number thats remaining to be added
+  for loop through the remaing set of numbers
 */
 
 // You can run `node index.js` to view these console logs
